9169ccdfb8fcbe59f4d6981354e4ead81b4dc311
Aleksandra Kolianichenko <mirvada1@yandex.ru>
2024-12-04 15:34:52 +0300

    добавляет список товаров на страницу

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import {Container, Title, TopBar, Filters } from "@/components/shared";
+import {Container, Title, TopBar, Filters, ProductsGroupList} from "@/components/shared";
 
 export default function Home() {
   return (
@@ -8,13 +8,33 @@ export default function Home() {
      </Container>
      <TopBar />
      <Container className='mt-10 pb-14'>
-       <div className='flex gap-[60px]'>
+       <div className='flex gap-[80px]'>
          <div className='w-[250px]'>
            <Filters />
          </div>
          <div className='flex-1'>
            <div className='flex flex-col gap-16'>
-             Список товаров
+             <ProductsGroupList title='Пиццы' products={[{
+               id: 1,
+               name: 'Бургер-пицца',
+               imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D61698827EE9B8DB6D0AEC53410.avif',
+               price: 550,
+               items: [{price: 500}],
+             }]} categoryId={1} />
+             <ProductsGroupList title='Комбо' products={[{
+               id: 1,
+               name: 'Бургер-пицца',
+               imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D61698827EE9B8DB6D0AEC53410.avif',
+               price: 550,
+               items: [{price: 500}],
+             }]} categoryId={1} />
+             <ProductsGroupList title='Закуски' products={[{
+               id: 1,
+               name: 'Бургер-пицца',
+               imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D61698827EE9B8DB6D0AEC53410.avif',
+               price: 550,
+               items: [{price: 500}],
+             }]} categoryId={1} />
            </div>
          </div>
        </div>
